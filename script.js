94552755d76756ea4c225331914c0ca959db4ec3
jpcp0614 <jpcp0614@gmail.com>
2023-08-14 19:12:28 -0300

    :sparkles: feat - Adiciona Mapa de palco e Portfolio

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
 const toogleMode = () => {
-  const html = document.documentElement;
-  html.classList.toggle('light')
+	const html = document.documentElement;
+	html.classList.toggle('light');
 
-  // pegar a tag img
-  // const img = document.querySelector("#profile img");
+	// pegar a tag img
+	// const img = document.querySelector("#profile img");
 
-  // if(html.classList.contains('light')){
-  //   img.setAttribute('src', './assets/avatar-light.JPG')
-  // } else {
-  //   img.setAttribute('src', './assets/avatar.png') 
-  // }
-}
+	// if(html.classList.contains('light')){
+	//   img.setAttribute('src', './assets/avatar-light.JPG')
+	// } else {
+	//   img.setAttribute('src', './assets/avatar.png')
+	// }
+};
+
+// Mapa de Palco
+const mapa_palco = document.querySelector('#mapa-de-palco');
+mapa_palco.addEventListener('click', (e) => {
+	e.preventDefault();
+	window.open('assets/mapa_de_palco.jpg', '_blank');
+});
+
+// Portfolio
+const portfolio = document.querySelector('#portfolio');
+portfolio.addEventListener('click', (e) => {
+	e.preventDefault();
+	window.open('assets/portfolio.pdf', '_blank');
+});
